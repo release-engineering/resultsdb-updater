76880d18ea0d37c8061998e70bc797d68b5272be
mprahl <matt_prahl@live.com>
2018-02-13 11:21:39 -0500

    Fix flake8 errors

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ with open('requirements.txt', 'r') as f:
 setup(
     name='resultsdb-updater',
     version='3.0.0',
-    description='A micro-service that listens for messages on the message bus and updates ResultsDB',
+    description=('A micro-service that listens for messages on the message '
+                 'bus and updates ResultsDB'),
     license='GPLv2+',
     author='Matt Prahl',
     author_email='mprahl@redhat.com',
