27023bf501e5e0e89fca8a82fd67b6699b8e4069
mprahl <matt_prahl@live.com>
2018-02-14 10:09:52 -0500

    Update to v3.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='resultsdb-updater',
-    version='3.0.0',
+    version='3.1.1',
     description=('A micro-service that listens for messages on the message '
                  'bus and updates ResultsDB'),
     license='GPLv2+',
