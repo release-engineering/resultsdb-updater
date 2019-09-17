f5fd61799f40109f50d58d738de8e9ca9b9bb0be
Lukas Holecek <hluk@email.cz>
2019-09-17 07:39:42 +0200

    Release v5.8.0
    Signed-off-by: Lukas Holecek <hluk@email.cz>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='resultsdb-updater',
-    version='5.7.0',
+    version='5.8.0',
     description=('A micro-service that listens for messages on the message '
                  'bus and updates ResultsDB'),
     license='GPLv2+',
